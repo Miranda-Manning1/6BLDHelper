5a3d9f841e8066bd19f6bc664ec3f9c863653910
Miranda-M <104934843+Miranda-Manning1@users.noreply.github.com>
2025-07-29 23:14:32 -0400

    Generalized memo creation

--- 6BLDHelper.py
+++ 6BLDHelper.py
@@ -83,7 +83,7 @@ def add_spaces_to_memo(s):
     return ' '.join(s[i:i + 2] for i in range(0, len(s), 2))
 
 # is a piece solved based on its relative index
-def color_in_correct_index(color, index):
+def color_on_correct_side(color, index):
     if index in [0, 1, 2, 3] and color == "W":
         return True
     if index in [4, 5, 6, 7] and color == "O":
@@ -99,44 +99,48 @@ def color_in_correct_index(color, index):
 
     return False
 
-def solve_outer_x_centers(outer_x_centers, buffer):
+def solve_pieces(piece_list, buffer):
     memo = ""
-    buffer_color = outer_x_centers[buffer]
+    buffer_color = piece_list[buffer]
     memo_finished = False
 
     while not memo_finished:
 
+        # decide what side to swap the buffer location piece to depending on the color of the piece in the buffer position.
+        # make sure that the buffer position is not in the list, because we never want to attempt to swap to the buffer.
         pieces_to_attempt_swap_to = []
         match buffer_color:
             case "W":
-                pieces_to_attempt_swap_to = [0, 1, 2, 3]
+                pieces_to_attempt_swap_to = [i for i in range(0, 4) if i != buffer]
             case "O":
-                pieces_to_attempt_swap_to = [4, 5, 6, 7]
+                pieces_to_attempt_swap_to = [i for i in range(4, 8) if i != buffer]
             case "G":
-                pieces_to_attempt_swap_to = [8, 9, 10, 11]
+                pieces_to_attempt_swap_to = [i for i in range(8, 12) if i != buffer]
             case "R":
-                pieces_to_attempt_swap_to = [12, 13, 14, 15]
+                pieces_to_attempt_swap_to = [i for i in range(12, 16) if i != buffer]
             case "B":
-                pieces_to_attempt_swap_to = [16, 17, 18, 19]
+                pieces_to_attempt_swap_to = [i for i in range(16, 20) if i != buffer]
             case "Y":
-                pieces_to_attempt_swap_to = [20, 21, 22, 23]
+                pieces_to_attempt_swap_to = [i for i in range(20, 24) if i != buffer]
 
         # check all 4 pieces on the side for an unsolved piece
         for potential_piece_to_swap_to in pieces_to_attempt_swap_to:
-            if not outer_x_centers[potential_piece_to_swap_to] == buffer_color:
-
+            if not piece_list[potential_piece_to_swap_to] == buffer_color:
                 # swap the colors of the buffer and the swapping piece
-                outer_x_centers[potential_piece_to_swap_to], buffer_color = buffer_color, outer_x_centers[potential_piece_to_swap_to]
+                piece_list[potential_piece_to_swap_to], buffer_color = buffer_color, piece_list[potential_piece_to_swap_to]
                 memo += number_to_letter(potential_piece_to_swap_to)
                 break
 
         # all 4 pieces on the side to swap to are already solved. this means it's time for a cycle break!
         else:
-            for i, potential_piece_color_to_swap_to in enumerate(outer_x_centers):
-                if not color_in_correct_index(potential_piece_color_to_swap_to, i):
+            for i, potential_piece_color_to_swap_to in enumerate(piece_list):
+                if i == buffer:
+                    continue  # don't try to swap with the buffer itself
+
+                if not color_on_correct_side(potential_piece_color_to_swap_to, i):
 
                     # swap the colors of the buffer and the swapping piece
-                    outer_x_centers[i], buffer_color = buffer_color, outer_x_centers[i]
+                    piece_list[i], buffer_color = buffer_color, piece_list[i]
                     memo += number_to_letter(i)
                     break
 
@@ -144,13 +148,14 @@ def solve_outer_x_centers(outer_x_centers, buffer):
             else:
                 memo_finished = True
 
-    return memo
+    return memo.upper()
 
 
 args = sys.argv[1:]
 
 if len(args) == 0:
-    scramble = scrambler666.get_WCA_scramble()
+    #scramble = scrambler666.get_WCA_scramble()
+    scramble = "Dw' Rw Fw2 F R 3Uw' Rw2 U Rw2 L F U Uw Bw D2 3Rw' Dw' L' Rw 3Uw' L B R Dw L2 Lw2 U' F' Lw Bw Rw2 3Rw F R2 L Dw2 Lw B2 R 3Rw 3Uw2 3Rw2 L' 3Uw Lw D' F 3Rw2 L2 Dw2 3Uw R' 3Fw' Bw D' R2 Fw' Uw' 3Rw2 U' F2 Rw 3Rw' Lw R2 Uw2 B F Uw2 L' U2 D2 Lw2 F' L R2 Lw 3Fw Dw' Uw"
     print("Scramble:")
     print(scramble)
     print()
@@ -158,27 +163,36 @@ if len(args) == 0:
     cube = magiccube.Cube(6)
     cube.rotate(scramble)
 
-    print()
-    print(cube)
-    print()
-
     parsed_cube = cube.get()
 
     # Reshape into 6 lists of 36
     cube_sides = [parsed_cube[i:i + 36] for i in range(0, 216, 36)]
 
-    print(get_outer_x_centers(cube_sides))
-    print(get_inner_x_centers(cube_sides))
-    print(get_clockwise_obliques(cube_sides))
-    print(get_counterclockwise_obliques(cube_sides))
-    print(get_outer_wings(cube_sides))
-    print(get_inner_wings(cube_sides))
-    print(get_corners(cube_sides))
+    outer_x_centers = get_outer_x_centers(cube_sides)
+    inner_x_centers = get_inner_x_centers(cube_sides)
+    clockwise_obliques = get_clockwise_obliques(cube_sides)
+    counterclockwise_obliques = get_counterclockwise_obliques(cube_sides)
+    outer_wings = get_outer_wings(cube_sides)
+    inner_wings = get_inner_wings(cube_sides)
+    corners = get_corners(cube_sides)
+
+    print(outer_x_centers)
+    print(outer_wings)
+    print(clockwise_obliques)
+    print(counterclockwise_obliques)
+    print(outer_wings)
+    print(inner_wings)
+    print(corners)
 
     outer_x_center_buffer = letter_to_number("a")
+    inner_x_center_buffer = letter_to_number("a")
 
-    x_center_memo = add_spaces_to_memo(solve_outer_x_centers(get_outer_x_centers(cube_sides), outer_x_center_buffer))
-    print(x_center_memo.upper())
+    full_memo = ""
+
+    full_memo += add_spaces_to_memo(solve_pieces(outer_x_centers, outer_x_center_buffer)) + "\n"
+    full_memo += add_spaces_to_memo(solve_pieces(inner_x_centers, inner_x_center_buffer)) + "\n"
+
+    print(full_memo)
 
     quit()
 
