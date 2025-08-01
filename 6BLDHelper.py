7037d7c36774f56d213d7cbfec31da07c3700d4e
Miranda-M <104934843+Miranda-Manning1@users.noreply.github.com>
2025-08-01 00:40:34 -0400

    Implemented outer wings solver

--- 6BLDHelper.py
+++ 6BLDHelper.py
@@ -4,6 +4,12 @@ import magiccube
 
 piece_list_debugging = True
 
+class Piece:
+    def __init__(self):
+        self.color = ""
+        self.side_color = ""
+        self.piece_type = ""
+
 def print_help():
     print("Welcome to the 6BLD Memo Maker!")
     print("-s [scramble] (default: makes its own scramble)")
@@ -40,34 +46,38 @@ def parse_cube(cube_str):
 
     return cube
 
-def get_pieces(sides, piece_locations):
-    pieces = ""
+def get_pieces(sides, piece_locations, piece_type):
+    pieces = []
     for side in sides:
-        for piece in piece_locations:
-            pieces += side[piece]
+        for piece_loc in piece_locations:
+            piece = Piece()
+            piece.color = side[piece_loc]
+            piece.piece_type = piece_type
+
+            pieces.append(piece)
 
     return list(pieces)
 
 def get_outer_x_centers(sides):
-    return get_pieces(sides, [7, 10, 28, 25])
+    return get_pieces(sides, [7, 10, 28, 25], "center")
 
 def get_inner_x_centers(sides):
-    return get_pieces(sides, [14, 15, 21, 20])
+    return get_pieces(sides, [14, 15, 21, 20], "center")
 
 def get_clockwise_obliques(sides):
-    return get_pieces(sides, [9, 22, 26, 13])
+    return get_pieces(sides, [9, 22, 26, 13], "center")
 
 def get_counterclockwise_obliques(sides):
-    return get_pieces(sides, [8, 16, 27, 19])
+    return get_pieces(sides, [8, 16, 27, 19], "center")
 
 def get_outer_wings(sides):
-    return get_pieces(sides, [4, 29, 31, 6])
+    return get_pieces(sides, [4, 29, 31, 6], "outer wing")
 
 def get_inner_wings(sides):
-    return get_pieces(sides, [3, 23, 32, 12])
+    return get_pieces(sides, [3, 23, 32, 12], "inner wing")
 
 def get_corners(sides):
-    return get_pieces(sides, [0, 5, 35, 30])
+    return get_pieces(sides, [0, 5, 35, 30], "corner")
 
 # gets the index of the buffer
 def letter_to_number(letter):
@@ -101,10 +111,23 @@ def color_on_correct_side(color, index):
 
     return False
 
-def get_piece_list_matrix(piece_list):
+def get_piece_color_matrix(piece_list):
     if len(piece_list) != 24:
         raise ValueError("Input list must have exactly 24 elements.")
-    return [piece_list[i:i+4] for i in range(0, 24, 4)]
+    return [[piece.color for piece in piece_list[i:i+4]] for i in range(0, 24, 4)]
+
+
+def give_outer_wings_side_colors(wings, sides):
+    side_colors = [
+        [4, 1], [3, 1], [2, 1], [1, 1], [0, 24], [2, 24], [5, 24], [4, 11],
+        [0, 34], [3, 24], [5, 1], [1, 11], [0, 11], [4, 24], [5, 11], [2, 11],
+        [0, 1], [1, 24], [5, 34], [3, 11], [2, 34], [3, 34], [4, 34], [1, 34]
+    ]
+
+    for i, wing in enumerate(wings):
+        side = side_colors[i][0]
+        index = side_colors[i][1]
+        wing.side_color = sides[side][index]
 
 
 # decide what side to swap the buffer location piece to depending on the color of the piece in the buffer position.
@@ -124,37 +147,60 @@ def get_pieces_to_attempt_swap_to(buffer, buffer_color):
         case "Y":
             return [i for i in range(20, 24) if i != buffer]
 
-def solve_center_pieces(piece_list, buffer):
+def check_outer_wing(piece, piece_location, assertion):
+    if piece.piece_type != "outer wing":
+        return False
+
+    correct_color = ""
+    if piece_location in [4,8,12,16]:
+        correct_color = "W"
+    if piece_location in [3,11,17,23]:
+        correct_color = "O"
+    if piece_location in [2,5,15,20]:
+        correct_color = "G"
+    if piece_location in [1,9,19,21]:
+        correct_color = "R"
+    if piece_location in [0,7,13,22]:
+        correct_color = "B"
+    if piece_location in [6,10,14,18]:
+        correct_color = "Y"
+
+    return (piece.side_color == correct_color) == assertion
+
+def solve_pieces(piece_list, buffer):
     memo = ""
     memo_finished = False
 
     piece_list_matrix = []
-    if piece_list_debugging: piece_list_matrix = get_piece_list_matrix(piece_list)
+    if piece_list_debugging: piece_color_matrix = get_piece_color_matrix(piece_list)
 
     while not memo_finished:
 
-        pieces_to_attempt_swap_to = get_pieces_to_attempt_swap_to(buffer, piece_list[buffer])
+        pieces_to_attempt_swap_to = get_pieces_to_attempt_swap_to(buffer, piece_list[buffer].color)
 
         # check all 4 pieces on the side for an unsolved piece
         for potential_piece_to_swap_to in pieces_to_attempt_swap_to:
-            if not piece_list[potential_piece_to_swap_to] == piece_list[buffer]:
+            if (not piece_list[potential_piece_to_swap_to].color == piece_list[buffer].color and piece_list[buffer].piece_type == "center") or (check_outer_wing(piece_list[buffer], potential_piece_to_swap_to, True) and piece_list[buffer].piece_type == "outer wing"):
 
                 # swap the colors of the buffer and the swapping piece
-                piece_list[potential_piece_to_swap_to], piece_list[buffer] = piece_list[buffer], piece_list[potential_piece_to_swap_to]
+                piece_list[potential_piece_to_swap_to].color, piece_list[buffer].color = piece_list[buffer].color, piece_list[potential_piece_to_swap_to].color
+                piece_list[potential_piece_to_swap_to].side_color, piece_list[buffer].side_color = piece_list[buffer].side_color, \
+                piece_list[potential_piece_to_swap_to].side_color
 
                 memo += number_to_letter(potential_piece_to_swap_to)
                 break
 
         # all 4 pieces on the side to swap to are already solved. this means it's time for a cycle break!
         else:
-            for i, potential_piece_color_to_swap_to in enumerate(piece_list):
+            for i, potential_piece_to_swap_to in enumerate(piece_list):
                 if i == buffer:
                     continue  # don't try to swap with the buffer itself
 
-                if not color_on_correct_side(potential_piece_color_to_swap_to, i):
+                if not color_on_correct_side(potential_piece_to_swap_to.color, i) or check_outer_wing(potential_piece_to_swap_to, i, False):
 
                     # swap the colors of the buffer and the swapping piece
-                    piece_list[i], piece_list[buffer] = piece_list[buffer], piece_list[i]
+                    piece_list[i].color, piece_list[buffer].color = piece_list[buffer].color, piece_list[i].color
+                    piece_list[i].side_color, piece_list[buffer].side_color = piece_list[buffer].side_color, piece_list[i].side_color
 
                     memo += number_to_letter(i)
                     break
@@ -162,6 +208,8 @@ def solve_center_pieces(piece_list, buffer):
             # there are no unsolved pieces - that means the memo is finished!
             else:
                 memo_finished = True
+
+        if piece_list_debugging: piece_color_matrix = get_piece_color_matrix(piece_list)
 
     return memo.upper()
 
@@ -187,16 +235,10 @@ if len(args) == 0:
     clockwise_obliques = get_clockwise_obliques(cube_sides)
     counterclockwise_obliques = get_counterclockwise_obliques(cube_sides)
     outer_wings = get_outer_wings(cube_sides)
-    inner_wings = get_inner_wings(cube_sides)
-    corners = get_corners(cube_sides)
+    #inner_wings = get_inner_wings(cube_sides)
+    #corners = get_corners(cube_sides)
 
-    print(outer_x_centers)
-    print(outer_wings)
-    print(clockwise_obliques)
-    print(counterclockwise_obliques)
-    print(outer_wings)
-    print(inner_wings)
-    print(corners)
+    give_outer_wings_side_colors(outer_wings, cube_sides)
 
     outer_x_center_buffer = letter_to_number("a")
     inner_x_center_buffer = letter_to_number("a")
@@ -208,12 +250,12 @@ if len(args) == 0:
 
     full_memo = ""
 
-    full_memo += add_spaces_to_memo(solve_center_pieces(outer_x_centers, outer_x_center_buffer)) + "\n"
-    full_memo += add_spaces_to_memo(solve_center_pieces(inner_x_centers, inner_x_center_buffer)) + "\n"
-    full_memo += add_spaces_to_memo(solve_center_pieces(clockwise_obliques, clockwise_oblique_buffer)) + "\n"
-    full_memo += add_spaces_to_memo(solve_center_pieces(counterclockwise_obliques, counterclockwise_oblique_buffer))
-    #full_memo += add_spaces_to_memo(solve_pieces(outer_wings, outer_wings_buffer, []))
-    #full_memo += add_spaces_to_memo(solve_pieces(inner_wings, inner_wings_buffer))
+    full_memo += add_spaces_to_memo(solve_pieces(outer_x_centers, outer_x_center_buffer)) + "\n"
+    full_memo += add_spaces_to_memo(solve_pieces(inner_x_centers, inner_x_center_buffer)) + "\n"
+    full_memo += add_spaces_to_memo(solve_pieces(clockwise_obliques, clockwise_oblique_buffer)) + "\n"
+    full_memo += add_spaces_to_memo(solve_pieces(counterclockwise_obliques, counterclockwise_oblique_buffer)) + "\n"
+    full_memo += add_spaces_to_memo(solve_pieces(outer_wings, outer_wings_buffer)) + "\n"
+    #full_memo += add_spaces_to_memo(solve_edge_pieces(inner_wings, inner_wings_buffer)) + "\n"
 
     print(full_memo)
 
